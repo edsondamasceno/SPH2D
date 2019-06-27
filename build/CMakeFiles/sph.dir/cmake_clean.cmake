15531b5d218b3145fbb3a125b86a882029422b4a
edsondamasceno <edsondamasceno@usp.br>
2019-06-27 10:23:02 -0300

    Add files

---
+++ build/CMakeFiles/sph.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sph.dir/main.cpp.o"
+  "CMakeFiles/sph.dir/scrInteractor.cpp.o"
+  "CMakeFiles/sph.dir/TrackBall.cpp.o"
+  "sph.pdb"
+  "sph"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
